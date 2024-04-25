4b272378bc94384c38911781684647c17ead94b0
MarcosCianci <marcos.cianci@gmail.com>
2024-04-25 20:18:41 -0300

    fix: disabled iam_instance_profile for test localhost

--- tests/main.tf
+++ tests/main.tf
@@ -9,7 +9,7 @@ module "ec2" {
     source                  = "../"
 
     ec2_name                = "teste"
-    iam_instance_profile    = "instance.profile"
+    #iam_instance_profile    = "instance.profile"
     ami                     = "ami-12114i34ui3o4"
     #monitoring              = true 
     #vpc_security_group_ids  = ["sg-0cdkdcldjsnjks"]
